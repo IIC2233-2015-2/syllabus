74393f98e99f9ec7f391a20e8c8f4db8f20622f3
indonoso <ivania_donoso@hotmail.com>
2015-09-07 18:39:28 -0300

    Ayudantía 4

---
+++ Ayudantías/AY04/ejemplosDecoradores.py
@@ -0,0 +1,51 @@
+__author__ = 'Ivania Donoso'
+
+# Las funciones son objetos, se pueden pasar como parámetros y recibir como resultados
+
+
+# def foobar(funcion_original):
+# print("estoy creando una función")
+#
+#     def nueva_function():
+#         print("Soy la nueva función que no puede sumar")
+#
+#     print(funcion_original(1, 2))
+#
+#     return nueva_function
+#
+#
+# def sumar(a, b):
+#     return a + b
+#
+# funcion = foobar(sumar)
+# funcion()
+# print(sumar(1, 4))
+
+
+def verbose(funcion_original):
+    def new_function(*args, **kwargs):
+        print("Entrando ", funcion_original.__name__)
+        funcion_original(*args, **kwargs)
+        print("Saliendo  ", funcion_original.__name__)
+
+    return new_function
+
+
+def widget_func():
+    print("Soy el widget")
+
+talkative_widget_func = verbose(widget_func)
+talkative_widget_func()
+
+# También los podemos guardar con el mismo nombre que la función original
+# widget_func = verbose(widget_func)
+# widget_func()
+
+# Podemos usar "sugar syntax"
+# @verbose
+
+
+
+
+
+
---
+++ Ayudantías/AY04/ejemplosDecoradores2.py
@@ -0,0 +1,51 @@
+__author__ = 'Ivania Donoso'
+
+
+def saludar_con_emocion(emocion, intensidad):
+    def agregar_amor(f):
+        print("Agregamos Amor")
+
+        def nuevo_saludo(*args):
+            print("Voy a saludar con amor")
+            f(*args)
+            print("{0} {1}".format(emocion, intensidad))
+            print("")
+        return nuevo_saludo
+    print("Se ha construído el decorador")
+    return agregar_amor
+
+
+@saludar_con_emocion("Te quiero", "mucho")
+def saludar(objeto, sujeto):
+    print('Hola {0}, soy {1} tu nuevo perro'.format(sujeto, objeto))
+
+print("Después de agregar amor al saludo")
+
+print("Vamos a llamar a P-chan para que salude")
+
+saludar('P-chan', 'Ivania')
+print("Despúes de que saluda a Ivania")
+saludar('P-chan', 'Pato')
+print("Después de que saluda a Pato")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
