76d6eafaf60f444bc3cab6a920c6b9b92337c540
Belén Saldias <bcsaldias@uc.cl>
2015-09-21 19:51:54 -0300

    Ac Abstract

---
+++ Actividades/AC11/Enunciado/main.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+# Implementar Superheroe, Wolverine y Storm
+
+
+if __name__ == '__main__':
+    # Realizar interacción
+    # valores recomendados para wolvering(hp= 50, fuerza= 20, regeneracion= 8)
+    # valores recomendados para storm(hp= 50, fuerza= 15, regeneracion= 10)
+    pass
