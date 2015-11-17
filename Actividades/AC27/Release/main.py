75a8f4975ce92ccd59bca9b3b5920860a75179ee
Marco Antonio Bucchi <marcocacola@gmail.com>
2015-11-17 10:36:35 -0300

    Publicada AC27

---
+++ Actividades/AC27/Release/main.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+
+# Debe tener los atributos:
+# * _id (string)
+# * name (string)
+# * votes (diccionario string: int)
+class Table:
+    pass
+
+
+if __name__ == '__main__':
+    pass
